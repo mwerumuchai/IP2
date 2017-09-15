2983cae31c319f4d39824496ff19bd3be31e917c
mwerumuchai <mwerumuchai@gmail.com>
2017-09-15 13:47:33 +0300

    create meal-model

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>IP2</h1>
+    <h1>Meal tracker</h1>
   </div>
   `
 })
 
 export class AppComponent {
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Chicken Burger", "Ordered a salad on the side instead of fries.", 375),
+      new Meal("Ugali beef", "Didn't have enough meat.", 150),
+      new Meal("Pizza", "Had extra toppings, which I like.", 450)
+    ];
+  }
 
 }
