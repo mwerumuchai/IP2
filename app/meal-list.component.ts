908861cc10956a9662d81f677e6075717bf67e1d
mwerumuchai <mwerumuchai@gmail.com>
2017-09-18 09:35:04 +0300

    style page

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -12,7 +12,8 @@ import { CaloriesPipe } from './calories.pipe';
   pipes: [CaloriesPipe],
   directives: [MealComponent, EditMealDetailsComponent, NewMealComponent],
   template: `
-    <h3>Log your food:</h3>
+  <div class="foodlog">
+    <h3 class= "log">Log your food:</h3>
     <select (change)="onCaloriesChange($event.target.value)" class="filter form-control form-calories-select">
       <option value="all" selected="selected">All Meals</option>
       <option value="under500"> Less than 500 Calories</option>
@@ -25,6 +26,7 @@ import { CaloriesPipe } from './calories.pipe';
         [meal]="currentMeal">
       </meal-display>
     </div>
+    <div>
     <div>
       <edit-meal-details *ngIf="selectedMeal" [meal]="selectedMeal"></edit-meal-details>
     </div>
